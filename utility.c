8af80ad88ef7d19d873f0fe6c6c59300b159579b
Aryan-Singh-0301 <77905821+Aryan-Singh-0301@users.noreply.github.com>
2022-03-09 22:52:24 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -1,7 +1,10 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include <string.h>
-#include <stdbool.h>
 
 void environ() {
     // be able to support piping
@@ -14,4 +17,34 @@ void callEcho(char *s) {
 
 void pause() {
     system("read -p 'Press [Enter] key to start backup...' enter");
+}
+//change the current working directory
+void cd(char* pwd, char* path){
+    int changed_dir = chdir(path);
+    if (changed_dir == 0){
+        printf("Current working directory has been changed to: %s\n ", path);
+        getcwd(pwd, 256);
+    }
+    else{
+        printf("%s> The specified directory was not found\n", pwd);
+    }
+}
+//clear the screen
+void clr(){
+    system("cls||clear");
+}
+//display the contents of the specified directory
+void dir(char* path){
+    struct dirent *dir;
+    DIR *d = NULL;
+    d = opendir(path);
+    if(d == NULL){
+        printf("Could not find the directory specified by pathname: %s\n", path);
+    }
+    while ((dir == readdir(d))!= NULL){
+        printf("%s\n", dir->d_name);
+    }
+
+    closedir(d);
+
 }
