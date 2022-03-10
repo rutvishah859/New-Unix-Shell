ca4605942a2f1ef061ab2e71e1e9616f11449aef
Krutarth Dave <47645595+QuantumBatarang@users.noreply.github.com>
2022-03-10 12:02:33 -0500

    parsing and doing cd and dir commands

--- myshell.c
+++ myshell.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <io.h>
+#include <dirent.h>
 
 #ifdef _WIN32
 #include <io.h>
@@ -15,54 +16,110 @@
 #include "utility.c"
 
 #define BUFFER_LEN 256
+#define DELIM " \t\r\n\a"
 
-char* strsep(char** stringp, const char* delim)
-{
-  char* start = *stringp;
-  char* p;
+char currDir[pMax];
 
-  p = (start != NULL) ? strpbrk(start, delim) : NULL;
+int cd(char **args);
+int dir();
 
-  if (p == NULL)
-  {
-    *stringp = NULL;
-  }
-  else
-  {
-    *p = '\0';
-    *stringp = p + 1;
-  }
+char *builtin[] = {
 
-  return start;
+    "cd",
+    "dir"
+};
+
+int (*builtinFunc[]) (char **) = {
+    &cd,
+    &dir
+};
+
+int numBuiltIns(){
+    return sizeof(builtin) / sizeof(char *);
 }
 
-void parseComm(char* str, char** parsed){
+int dir(){
 
-    for (int i = 0; i < 100; i++){
-        
-        parsed[i] = strsep(&str, " ");
+    DIR *d;
 
-        if(parsed[i] == NULL){
-            break;
+    struct dirent *dir;
+
+    d = opendir(".");
+
+    if(d){
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
         }
+        closedir(d);
+    }
 
-        if(strlen(parsed[i]) == 0){
-            i--;
+    return(0);
+}
+
+int cd(char **args){
+
+    if (args[1] == NULL){
+        fprintf(stderr, "expected argument to \"cd\"\n");
+    }else {
+        if(chdir(args[1]) != 0){
+            perror("shell");
+        }else{
+            getcwd(currDir, pMax);
         }
     }
-    
-    
 
+    return 1;
 }
 
-int processCommand(char* str, char** parsed){
+int execute(char **args){
 
+    if(args[0] == NULL){
+        return 1;
+    }
 
-    parseComm(str, parsed);
+    for (int i = 0; i < numBuiltIns(); i++){
+        
+        if(strcmp(args[0], builtin[i]) == 0){
+            return (*builtinFunc[i])(args);
+        }
+    }
 
-    return 0;
-
+    return 1;
+    
 }
+
+char **splitComm(char *command){
+
+    int bufsize = BUFFER_LEN, pos = 0;
+
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if(!tokens){
+         fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(command, DELIM);
+  while (token != NULL) {
+    tokens[pos] = token;
+    pos++;
+
+    if (pos >= bufsize) {
+      bufsize += BUFFER_LEN;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, DELIM);
+  }
+  tokens[pos] = NULL;
+  return tokens;
+}
+
 
 int main(int argc, char *argv[]) {
 
@@ -70,7 +127,8 @@ int main(int argc, char *argv[]) {
 
     char currDir[pMax];
 
-    char *parsedArgs[pMax];
+    char **args;
+    
 
     int execFlag = 0;
 
@@ -78,15 +136,19 @@ int main(int argc, char *argv[]) {
     printf("%s$ ", currDir);
     while (fgets(command, BUFFER_LEN, stdin) != NULL){
 
+        char *parsedArgs[pMax];
+
         if(strcmp(command, "quit\n") == 0){
             break;
         }
 
-        parseComm(command, parsedArgs);
+        args = splitComm(command);
+
+        execute(args);
+
+        free(args);
 
         printf("%s$ ", currDir);
-
-        printf("%s %s %s\n", parsedArgs[0], parsedArgs[1], parsedArgs[2]);
         
         
 
