bf2fac8b878734510bc2ccaef3794c6ee635efc1
Rutvi Shah <rutvishah859@gmail.com>
2022-03-11 22:37:22 -0500

    added launch function

--- myshell.c
+++ myshell.c
@@ -97,7 +97,32 @@ int execute(char **args){
         }
     }
 
-    return 1;
+    return launch(args);
+}
+
+int launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
 }
 
 // splits command at space to get each token of the command
